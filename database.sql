542549f67c25df87f8acf69ee37d2a9b4f70b007
0918nobita <nobita.0918@gmail.com>
2018-08-16 07:04:48 +0900

    Add database.sql

---
+++ database.sql
@@ -0,0 +1,12 @@
+-- ユーザー情報を格納するテーブルを作成
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `password` char(64) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- サンプルレコードの挿入
+INSERT INTO `users` (`name`, `password`) VALUES ('Matsumoto', 'E47089C9E704B549DD596AFD4345349A07533A0DD5021FB56D666DDB728B84C2');
