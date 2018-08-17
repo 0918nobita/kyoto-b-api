b73466269ef0d5a99a81ff8eb376bd26c8c860e6
0918nobita <nobita.0918@gmail.com>
2018-08-17 19:33:41 +0900

    UsersModelクラスでModelインターフェースを実装した

--- api/src/main/kotlin/net/zero918nobita/api/models/UsersModel.kt
+++ api/src/main/kotlin/net/zero918nobita/api/models/UsersModel.kt
@@ -1,21 +1,23 @@
 package net.zero918nobita.api.models
 
 import net.zero918nobita.api.entities.User
+import java.sql.ResultSet
 
-object UsersModel {
+object UsersModel: Model<User> {
   fun findAllUsers(): List<User> {
     val stmt = Database.connection.createStatement()
     stmt.executeQuery("SELECT * FROM users")
-    val resultSet = stmt.resultSet
+    return stereotype(stmt.resultSet)
+  }
 
+  override fun stereotype(resultSet: ResultSet): List<User> {
     val result = mutableListOf<User>()
     while (resultSet.next()) {
       result.add(User(userId = resultSet.getLong(1),
-                      userName = resultSet.getString(2),
-                      userScreenName = resultSet.getString(3),
-                      password = resultSet.getString(4)))
+              userName = resultSet.getString(2),
+              userScreenName = resultSet.getString(3),
+              password = resultSet.getString(4)))
     }
-
     return result
   }
 }
