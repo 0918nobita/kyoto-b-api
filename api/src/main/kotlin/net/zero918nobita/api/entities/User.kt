222bbc2f3b8bafcd1356f1c93a3818fab97d4e4a
0918nobita <nobita.0918@gmail.com>
2018-08-17 18:06:35 +0900

    Userデータクラスを追加した

---
+++ api/src/main/kotlin/net/zero918nobita/api/entities/User.kt
@@ -0,0 +1,6 @@
+package net.zero918nobita.api.entities
+
+data class User(val id: Long,
+                val userName: String,
+                val userScreenName: String,
+                val password: String)
